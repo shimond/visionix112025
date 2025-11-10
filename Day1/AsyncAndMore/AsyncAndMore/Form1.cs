518b29092b8207cb0b9be1b6cf7387a0f5559597
Shimon Dahan <shimond@any-techs.co.il>
2025-11-10 13:14:57 +0200

    Using async

--- Day1/AsyncAndMore/AsyncAndMore/Form1.cs
+++ Day1/AsyncAndMore/AsyncAndMore/Form1.cs
@@ -7,7 +7,7 @@ namespace AsyncAndMore
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
             string[] urls = new string[]
             {
@@ -19,31 +19,37 @@ namespace AsyncAndMore
             int sum = 0;
             foreach (string url in urls)
             {
-                listBoxLogs.Items.Add($"Start Download from  {url}");
-                DownloadUrl(url, bytes =>
+                try
                 {
+                    listBoxLogs.Items.Add($"Start Download from  {url}");
+                    var bytes = await DownloadUrl(url);
                     sum += bytes;
                     listBoxLogs.Items.Add($"Downloaded {bytes} bytes from {url}");
-                }, ex =>
+
+                }
+                catch (Exception ex)
                 {
                     listBoxLogs.Items.Add($"Failed Downloaded from  {url} -  {ex.Message}");
-                });
-            }                
+                }
+            }
 
             this.Text = $"Total bytes downloaded: {sum}";
         }
 
-        private void DownloadUrl(string url, Action<int> OnSucess, Action<Exception> onError)
+        private Task<int> DownloadUrl(string url)
         {
-            new Thread(()=> {
+            return Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(Random.Shared.Next(2000, 10000));
                 if (url.Contains("github"))
                 {
-                    //throw new Exception("Simulated download error.");
-                    onError(new Exception("Simulated download error."));
+                    throw new Exception("Simulated download error.");
                 }
-                Thread.Sleep(Random.Shared.Next(2000, 10000)); // Simulate a delay
-                OnSucess(Random.Shared.Next(200000, 400000));
-            }).Start();
+                return Random.Shared.Next(200000, 400000);
+
+            });
+            
+
         }
     }
 }
